bdfffba0f93153cbff031303d2b802d3ba78006d
Aizen Baidya <aizen.shiratani.baidya@gmail.com>
2024-10-06 14:55:05 -0700

    Image on 'Game' page now shows :)

--- app/src/pages/Game.jsx
+++ app/src/pages/Game.jsx
@@ -1,10 +1,12 @@
+import gameImage from '../images/game.png';
+
 export default function Game() {
   return (
     <div className="text-center">
       <h2>Average BPM 120</h2>
       <h3>30 Minutes</h3>
       <h1>Loading Game ...</h1>
-      <img src="/images/game.png" alt="game art" />
+      <img src={gameImage} style={{width : "30%"}}alt="game art" />
     </div>
   );
 }
