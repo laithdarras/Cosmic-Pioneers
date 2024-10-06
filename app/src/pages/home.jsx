e4a3b60bc6f2eb16ee9b408bbec77ad54f6e243f
Parth Jain <parthgmjain@gmail.com>
2024-10-06 15:51:03 -0700

    styling home page and connecting it to begin page

--- app/src/pages/home.jsx
+++ app/src/pages/home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import "./home.css";
+import { Link } from "react-router-dom";
 // import './home.css';
 import { useState, useEffect } from "react";
 
 export default function Home() {
   
-
   return (
     <div className="home">
       <header>
@@ -14,26 +15,36 @@ export default function Home() {
         <section className="features">
           <br></br>
           <h2 align="center">Features</h2>
-            <p align="center">Engaging Microgravity Fitness Game</p>
-            <p align="center">Track Your Progress</p>
-            <p align="center">Build Team Rapport</p>
-            <p align="center">Multiple Earth and Space Based Themes</p>
-            <p align="center">Test your might against other Space Stations</p>
-            <p align="center">Journal your progress</p>
+            <div class="text-box col-auto" align = "center">Engaging Microgravity Fitness Game</div>
+            <br></br>
+            <div class="text-box" align = "center">Track Your Progress</div>
+            <br></br>
+            <div class="text-box" align = "center">Build Team Rapport</div>
+            <br></br>
+            <div class="text-box" align = "center">Multiple Earth and Space Based Themes</div>
+            <br></br>
+            <div class="text-box" align = "center">Test your might against other Space Stations</div>
+            <br></br>
+            <div class="text-box" align = "center">Journal your progress</div>
         </section>
        
         <section className="call-to-action">
+        <div class="icon"></div>
           <br></br>
           <h2 align="center">Join Us!</h2>
           <p align="center">Ready to launch into fun and fitness?</p>
+          <Link to="/begin">
           <div className="text-center">
             <button type="button" class="btn btn-primary btn-lg">Start Your Journey</button> 
           </div>
+          </Link>
+          <div class="iconss"></div>
+        
           
         </section>
       </main>
       <footer>
-        <p>© 2024 Cosmic Pioneers. All rights reserved.</p>
+      <div class="bottomleft">2024 Cosmic Pioneers. All rights reserved.</div>
       </footer>
     </div>
   );
