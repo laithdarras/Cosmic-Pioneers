4541669c56ba0f82ac5e9e7089e8c387287ef5f2
Laith Darras <laith.s.darras@gmail.com>
2024-10-06 16:22:03 -0700

    implemented graph analytics mood tracker feature

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -5,11 +5,13 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 //import bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 import Home from "./pages/home";
 import Music from "./pages/Music";
 import Journal from "./pages/Journal";
-import Timer from "./pages/Timer";
+import Begin from "./pages/Begin";
+import Game from "./pages/Game";
 
 //define accessible routes and which componenets respond to which URL
 const router = createBrowserRouter([
@@ -27,12 +29,17 @@ const router = createBrowserRouter([
         element: <Music />,
       },
       {
+        path: "/game",
+        element: <Game />,
+      },
+
+      {
         path: "/journal",
         element: <Journal />,
       },
       {
-        path: "/timer",
-        element: <Timer />,
+        path: "/begin",
+        element: <Begin />,
       },
     ],
   },
---
+++ app/src/pages/Game.jsx
@@ -0,0 +1,10 @@
+export default function Game() {
+  return (
+    <div className="text-center">
+      <h2>Average BPM 120</h2>
+      <h3>30 Minutes</h3>
+      <h1>Loading Game ...</h1>
+      <img src="/images/game.png" alt="game art" />
+    </div>
+  );
+}
